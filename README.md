e9efaffe3667ce8c2c1a383e8ba64feeff027a23
Johan Brook <johan.jvb@gmail.com>
2014-11-23 13:55:29 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Songs showcase
+
+Plain, static HTML and hand-written JSON. Ftw.
+
+## Develop
+
+```bash
+$ npm install
+$ gulp watch
+$ open index.html
+```
+
+Created with all the love in the world by Johan.
---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+var gulp =        require('gulp'),
+    sass =        require('gulp-sass'),
+    minify =      require('gulp-minify-css'),
+    handlebars =  require('gulp-compile-handlebars'),
+    data =        require('gulp-data'),
+    rename =      require('gulp-rename')
+
+gulp.task('template', function() {
+  return gulp.src('template.hbs')
+        .pipe(data(function() {
+          return require('./data.json')
+        }))
+        .pipe(handlebars())
+        .pipe(rename('index.html'))
+        .pipe(gulp.dest('./'))
+})
+
+gulp.task('css', function() {
+  gulp.src('stylesheets/master.scss')
+      .pipe(sass())
+      .pipe(minify({ cache: true }))
+      .pipe(gulp.dest('./css'))
+})
+
+gulp.task('watch', function() {
+  gulp.watch('./template.hbs', ['template'])
+  gulp.watch('./stylesheets/*.scss', ['css'])
+})
+
+gulp.task('default', ['data'])
