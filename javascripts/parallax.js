97eb53fd23ab9ddd87de6affd5f0721d65d313aa
Johan Brook <johan.jvb@gmail.com>
2014-12-09 19:59:23 +0100

    Remove throttle

--- javascripts/parallax.js
+++ javascripts/parallax.js
@@ -5,37 +5,6 @@
     By Johan Brook (@johanbrook), 2014.
   */
 
-  // From Underscore
-  var throttle = function(wait, func, options) {
-    var context, args, result
-    var timeout = null
-    var previous = 0
-    if (!options) options = {}
-    var later = function() {
-      previous = options.leading === false ? 0 : Date.now()
-      timeout = null
-      result = func.apply(context, args)
-      if (!timeout) context = args = null
-    }
-    return function() {
-      var now = Date.now()
-      if (!previous && options.leading === false) previous = now
-      var remaining = wait - (now - previous)
-      context = this
-      args = arguments
-      if (remaining <= 0 || remaining > wait) {
-        clearTimeout(timeout)
-        timeout = null
-        previous = now
-        result = func.apply(context, args)
-        if (!timeout) context = args = null
-      } else if (!timeout && options.trailing !== false) {
-        timeout = setTimeout(later, remaining)
-      }
-      return result
-    }
-  }
-
   var isTouch = function() {
     var platform = navigator.platform.toLowerCase()
     return platform.indexOf('ipad') != -1  ||  platform.indexOf('iphone') != -1
@@ -53,7 +22,7 @@
     var logo = document.querySelector('.parallax-logo'),
         header = document.querySelector('.parallax-header')
 
-    var scrollHandler = throttle(100, function(e) {
+    var scrollHandler = function(e) {
       var val = Math.max(window.pageYOffset, 0),
           offset = -(val * 0.5),
           opacity = (1 - (val / 500))
@@ -63,7 +32,7 @@
 
       transform(logo, offset*1.5)
       transform(header, offset)
-    })
+    }
 
     document.addEventListener('scroll', scrollHandler, false)
   }
